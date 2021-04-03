ac268934ceb8aa449abdb487ae6014ad7cbed092
puYulia <81847676+puYulia@users.noreply.github.com>
2021-04-03 10:04:58 +0300

    first version 3

--- module_0_final.py
+++ module_0_final.py
@@ -1,29 +1,5 @@
 import numpy as np
 
-    count = 0                           # счетчик попыток
-    number = np.random.randint(1,101)   # загадали число
-    print ("Загадано число от 1 до 100")
-    
-    while True:                        # бесконечный цикл
-        predict = int(input())         # предполагаемое число
-        count += 1                     # плюсуем попытку
-        if number == predict: break    # выход из цикла, если угадали
-        elif number > predict: print (f"Угадываемое число больше {predict} ")
-        elif number < predict: print (f"Угадываемое число меньше {predict} ")
-            
-    print (f"Вы угадали число {number} за {count} попыток.")    
-
-def game_core_v1(number):
-    '''Просто угадываем на random, никак не используя информацию о больше или меньше.
-       Функция принимает загаданное число и возвращает число попыток'''
-    count = 0
-    while True:
-        count+=1
-        predict = np.random.randint(1,101) # предполагаемое число
-        if number == predict: 
-            return count # выход из цикла, если угадали
-        
-        
 def score_game(game_core):
     '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
     count_ls = []
@@ -34,4 +10,55 @@ def score_game(game_core):
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
     return(score)
+
+def game_core_v4(number):
+    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток'''
+    score_min = 1
+    score_max = 100
+    
+    count = 1
+    predict = int(round(score_max/2))
+
+    while number != predict:
+        count += 1
+        predict, score_min, score_max = iterstep(number, predict, score_min, score_max)
+
+    return(count) # выход из цикла, если угадали
+
+
+def iterstep(number, predict, score_min, score_max):
+    if number > predict: 
+        # Если искомое значение больше  
+        
+        # Меняем минимальный предел поиска на следующей итерации
+        score_min = predict
+
+        # Проверяем если предел поиска больше 2 (можно взять среднее значение диапазона), либо двигаемся пошагово
+        if (score_max - score_min) > 2:
+            predict_next = int(round(score_min + (score_max - score_min)/2))
+        else:
+            predict_next = predict + 1
+
+    
+    elif number < predict: 
+        # Если искомое значение меньше
+
+        # Меняем максимальный предел поиска на следующей итерации
+        score_max = predict
+
+        # Проверяем если предел поиска больше 2 (можно взять среднее значение диапазона), либо двигаемся пошагово
+        if (score_max - score_min) > 2:
+            predict_next = int(round(score_min + (score_max - score_min)/2))
+        else:
+            predict_next = predict - 1
+
+    else:
+        predict_next = number
+
+    # Возвращаем значения для следующего шага поиска
+    return predict_next, score_min, score_max
+
+
+score_game(game_core_v4)
 
