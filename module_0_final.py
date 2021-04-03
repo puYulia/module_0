69d3725123d9561b21e3f6bcf07b65536ffb4520
puYulia <81847676+puYulia@users.noreply.github.com>
2021-04-03 10:02:32 +0300

    first version 2

--- module_0_final.py
+++ module_0_final.py
@@ -12,3 +12,26 @@ import numpy as np
         elif number < predict: print (f"Угадываемое число меньше {predict} ")
             
     print (f"Вы угадали число {number} за {count} попыток.")    
+
+def game_core_v1(number):
+    '''Просто угадываем на random, никак не используя информацию о больше или меньше.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 0
+    while True:
+        count+=1
+        predict = np.random.randint(1,101) # предполагаемое число
+        if number == predict: 
+            return count # выход из цикла, если угадали
+        
+        
+def score_game(game_core):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1,101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
+
